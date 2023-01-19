c1ac10a6ebbc056e59d9f8b40e0f98b7dfb23297
Benedict <ailnk@naver.com>
2023-01-19 16:58:43 +0900

    install vue3.5, pinia, vitest, firebase9

--- src/main.js
+++ src/main.js
@@ -1,6 +1,37 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import './assets/main.css'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBIF7QwsEZ2hj2_OJSjHgD6kr-vAYi27Io",
+    authDomain: "board-connect.firebaseapp.com",
+    projectId: "board-connect",
+    storageBucket: "board-connect.appspot.com",
+    messagingSenderId: "119315799004",
+    appId: "1:119315799004:web:000480f797beae27d24d19",
+    measurementId: "G-KMWE4VGH6T"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig)
+export const firebase = {
+    analytics: getAnalytics(firebaseApp),
+    auth: getAuth(firebaseApp),
+    db: getFirestore(firebaseApp),
+    storage: getStorage(firebaseApp),
+}
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
