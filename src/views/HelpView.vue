c515923a6604edd8297348cbd4154220e312d49c
Benedict <ailnk@naver.com>
2023-05-18 13:45:36 +0900

    chore: debug for mobile local storage

--- src/views/HelpView.vue
+++ src/views/HelpView.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts">
-import HelloWorld from '../components/HelloWorld.vue'
-import TheFirebase from '../components/TheFirebase.vue'
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <header class="text-center py-5">
     <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
 
+    <div class="py-5">
+      <h1>localStorage</h1>
+      <p v-for="d in datas" :key="d.key">{{ d.key }} : {{ d.value }}</p>
+    </div>
     <div class="py-5">
       <HelloWorld msg="You did it!" />
     </div>
@@ -20,5 +18,23 @@ import TheWelcome from '../components/TheWelcome.vue'
     <TheWelcome />
   </main>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import HelloWorld from '../components/HelloWorld.vue'
+import TheFirebase from '../components/TheFirebase.vue'
+import TheWelcome from '../components/TheWelcome.vue'
+
+const datas = ref([{ key: '', value: '' }])
+
+datas.value.pop()
+const len = localStorage.length
+for (let i = 0; i < len; i++) {
+  let k: string = localStorage.key(i) || ''
+  let v: string = localStorage.getItem(k) || ''
+  datas.value.push({ key: k, value: v })
+}
+console.log(datas.value)
+</script>
 
 <style scoped></style>
