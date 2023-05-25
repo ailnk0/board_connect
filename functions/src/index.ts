a18baf313cf07edf1af2c6a219bde1f1bb7d8b13
Benedict <ailnk@naver.com>
2023-05-25 23:19:13 +0900

    chore: change region of firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,13 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { onRequest } from 'firebase-functions/v2/https'
+import functions = require('firebase-functions')
 import * as logger from 'firebase-functions/logger'
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
+exports.helloWorld = functions.region('asia-northeast3').https.onRequest((request, response) => {
   logger.info('Hello logs!', { structuredData: true })
   response.send('Hello from Firebase!')
 })
