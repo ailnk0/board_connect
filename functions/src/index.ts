2f2949b20ee8321870d72aa3d0df7ca3fbec3934
Benedict <ailnk@naver.com>
2023-05-31 22:43:58 +0900

    chore: configure chat gpt

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,7 +19,7 @@ import { Configuration, OpenAIApi } from 'openai'
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
-const openai = new OpenAIApi(configuration)
+const openAi = new OpenAIApi(configuration)
 
 exports.helloWorld = functions
   .region('asia-northeast1')
@@ -31,9 +31,24 @@ exports.helloWorld = functions
 exports.callGpt = functions
   .region('asia-northeast1')
   .https.onRequest(async (req: functions.https.Request, res: functions.Response<any>) => {
-    const completion = await openai.createCompletion({
+    const completion = await openAi.createCompletion({
       model: 'text-davinci-003',
       prompt: 'Hello world'
     })
     res.send(completion.data.choices[0].text)
+  })
+
+exports.callChatGpt = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (req: functions.https.Request, res: functions.Response<any>) => {
+    const completion = await openAi.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: 'Who won the world series in 2020?' },
+        { role: 'assistant', content: 'The Los Angeles Dodgers won the World Series in 2020.' },
+        { role: 'user', content: 'Where was it played?' }
+      ]
+    })
+    res.send(completion.data.choices[0].message)
   })
